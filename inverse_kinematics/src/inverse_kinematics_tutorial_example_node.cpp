a72d05a2f7094a99f4b2b708ed4233e95049a077
Sean <sean.maroofi@tuhh.de>
2021-05-29 20:41:46 +0200

    + Example of Tutorial 5 finished in inverse_kinematics_tutorial_example.cpp

--- inverse_kinematics/src/inverse_kinematics_tutorial_example_node.cpp
+++ inverse_kinematics/src/inverse_kinematics_tutorial_example_node.cpp
@@ -1,18 +1,21 @@
 /*
  * This is the example of Tutorial 5 to test and understand incremental inverse kinematics
  * author: Sean Maroofi
- * last edited: 25.5.21
+ * added: 25.5.21
+ * last edited: 26.5.21
  */
 #include <ros/ros.h>
 #include "sensor_msgs/JointState.h"
 #include <eigen3/Eigen/Dense>
+#include <stdlib.h>
 
 #define PI 3.14159265
 
 using namespace Eigen;
 
 // matrices and vectors for the equations
-static VectorXd initialTheta(3);
+static VectorXd initialThetas(3);
+static VectorXd desiredThetas(3);
 static Matrix4Xd initialA;
 static Matrix4Xd desiredA;
 
@@ -53,21 +56,52 @@ VectorXd convert4DMatrixTo12DVector(MatrixXd M){
 }
 
 /*
-void callback(const sensor_msgs::JointState& msg) {
-  std::array<MatrixXd, 8> a_;
-  for(int i  = 0; i<8; i++){
-    a_.at(i) = exampleMatrix_A_03(msg.position[i], a(i), d(i), alpha(i));
-  }
-  VectorXd in(4);
-  in << 0, 0, 0, 1;
-  VectorXd out = a_.at(0)*a_.at(1)*a_.at(2)*a_.at(3)*a_.at(4)*a_.at(5)*a_.at(6)*a_.at(7)*in;
-  std::cout << "End_pos: " << "\n" << "x: " << out(0) << "\n" << "y: " << out(1) << "\n" << "z: " << out(2) << "\n";
+ * This function gives the Jacobian of the example Matrix A. It takes the joint angles and returns a 12x3 Matrix.
+ *
+ * @param: float theta1, float theta2, float d3*
+ * returns: MatrixXd Jacobian
+ */
+MatrixXd jacobian (float theta1, float theta2, float d3){
+  MatrixXd Jacobi(12, 3);
+                  Jacobi <<  -sin(theta1)*cos(theta2)               , -sin(theta2)*cos(theta1)              , 0,
+                             cos(theta1)*cos(theta2)                , -sin(theta1)*sin(theta2)              , 0,
+                             0                                      , -cos(theta2)                          , 0,
+                             -cos(theta1)                           , 0                                     , 0,
+                             -sin(theta1)                           , 0                                     , 0,
+                             0                                      , 0                                     , 0,
+                             -sin(theta1)*sin(theta2)               , cos(theta1)*cos(theta2)               , 0,
+                             sin(theta2)*cos(theta1)                , sin(theta1)*cos(theta2)               , 0,
+                             0                                      , -sin(theta2)                          , 0,
+                             -(d3 + l2 + l3)*sin(theta1)*sin(theta2), (d3 + l2 +l3)*cos(theta1)*cos(theta2) , sin(theta2)*cos(theta1),
+                             (d3 + l2 + l3)*sin(theta2)*cos(theta1) , (d3 + l2 + l3)*sin(theta1)*cos(theta2), sin(theta1)*sin(theta2),
+                             0                                      , -(d3 + l2 + l3)*sin(theta2)           , cos(theta2);
+  return Jacobi;
 }
-*/
+
+/*
+ * This recursive function performs the incremental inverse kinematics. It takes the current joint angles, the current A and final A Matrix as 12x1 vectors. It returns the desired joint angles for the desired position of the robot
+ *
+ * @param: VectorXd thetas, VectorXd currentA. VectorXd finalA
+ * returns: VectorXd desiredThetas
+ */
+VectorXd incrementalStep(VectorXd currentThetas, VectorXd currentA, VectorXd finalA){
+  VectorXd delta_A = (finalA - currentA);
+
+  if (abs(delta_A.maxCoeff()) <= 0.01){
+      return currentThetas;
+    } else{
+      MatrixXd J = jacobian(currentThetas[0], currentThetas[1], currentThetas[2]);
+      MatrixXd pinvJ = J.completeOrthogonalDecomposition().pseudoInverse();
+
+      VectorXd nextThetas = currentThetas + pinvJ * delta_A/10;
+      VectorXd nextA = convert4DMatrixTo12DVector(exampleMatrix_A_03(nextThetas[0], nextThetas[1], nextThetas[2]));
+      return incrementalStep(nextThetas, nextA, finalA);
+    }
+}
 
 int main(int argc, char** argv)  {
   // initialize variables
-  initialTheta << 40*PI/180, 40*PI/180, 20;
+  initialThetas << 40*PI/180, 40*PI/180, 20;
   initialA = exampleMatrix_A_03(initialTheta[0],initialTheta[1], initialTheta[2]);
   desiredA << 0.5792, -0.7071, 0.4056,  93.2834,
               0.5792,  0.7071, 0.4056,  93.2834,
@@ -78,27 +112,10 @@ int main(int argc, char** argv)  {
   VectorXd initialA_vector = convert4DMatrixTo12DVector(initialA);
   VectorXd desiredA_vector = convert4DMatrixTo12DVector(desiredA);
 
+  // perform incremental steps
+  desiredThetas = incrementalStep(initialThetas, initialA_vector, desiredA_vector);
 
-  /*a << 0, 0, 0, 0.0825, -0.0825, 0, 0.088, 0;
-  d << 0.333, 0, 0.316, 0, 0.384, 0, 0, 0.107;
-  alpha << 0, -M_PI/2, M_PI/2, M_PI/2, -M_PI/2, M_PI/2, M_PI/2, 0;
-  ros::init(argc, argv, "forward_kin_node");
+  // final Matrix A
+  MatrixXd finalA = exampleMatrix_A_03(desiredThetas[0], desiredThetas[1], desiredThetas[2]);
 
-  // Get a node handle to the private parameters
-  // e.g. /node_name/parameter_name can then be
-  // read by nh.getParam("parameter_name", value);
-  ros::NodeHandle  nh("~");
-
-  // Parse parameters specified in the .launch file
-  std::string topic_name;
-  int queue_size;
-  nh.getParam("topic_name", topic_name);
-  nh.getParam("queue_size", queue_size);
-
-  // Register a callback function (a function that is called every time a new message arrives)
-  ros::Subscriber sub = nh.subscribe("/joint_states", 10, callback);
-
-  // Prevent the program from terminating
-  // Blocks but still allows messages to be received
-  ros::spin();*/
 }
