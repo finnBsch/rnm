e72351cef69b02a2b7d732c34fe1e651cf7a3f0e
nico <nicofischer@mail.de>
2021-06-04 15:47:33 +0200

    new frame extraction, waits for any key to be pressed

--- frame_reader/src/main.cpp
+++ frame_reader/src/main.cpp
@@ -10,102 +10,140 @@
 using namespace cv;
 using namespace std;
 
-int pose = 1;// just a counter for the filenames
+// if run, program waits until any key is pressed and then saves rgb, ir, depth images
+// to path + /frame_reader/cal_imgs/
 
-// frames to extract
-const int n_poses = 44; // number of different frames to extract
-int time_stamps[n_poses] = {1616052220, 1616052227, 1616052235, 1616052244, 1616052256,
-                            1616052267, 1616052276, 1616052284, 1616052293, 1616052303,
-                            1616052313, 1616052325, 1616052332, 1616052341, 1616052352,
-                            1616052363, 1616052373, 1616052379, 1616052387, 1616052396,
-                            1616052406, 1616052412, 1616052423, 1616052435, 1616052442,
-                            1616052454, 1616052465, 1616052477, 1616052489, 1616052498,
-                            1616052507, 1616052518, 1616052528, 1616052538, 1616052546,
-                            1616052555, 1616052566, 1616052575, 1616052583, 1616052592,
-                            1616052603, 1616052611, 1616052621, 1616052629};
-// list for already saved imgs, to avoid saving multiple images of same time stamp
-int time_stamps_saved[n_poses] = {};
+// package path
+string path = "/home/nico/CLionProjects/rnm_ws/src";
 
-// function for showing the images from sensor_imgs; default: not used
-void imageCallback(const sensor_msgs::ImageConstPtr& msg){
-//    const ros::Time time_stamp = msg->header.stamp;
+// counters for the filenames
+int pose_rgb = 1;
+int pose_ir = 1;
+int pose_depth = 1;
 
-// converting from ros to cv (mat) format, pointer to image from cv_bridge::CvImage
-  try{
-    cv::Mat img = cv_bridge::toCvShare(msg, "bgr8")->image;
-    cv::imshow("frame", img);
-    cv::waitKey(100);
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// conversion of depth image
+void depthToCV8UC1(const cv::Mat& float_img, cv::Mat& mono8_img) {
+  //Process images
+  if (mono8_img.rows != float_img.rows || mono8_img.cols != float_img.cols) {
+    mono8_img = cv::Mat(float_img.size(), CV_8UC1);
   }
-  catch (cv_bridge::Exception& e){
-    ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
-  }
-  //ROS_INFO("frame_id: %s ; time: %s", frame_id, time);
-  //ROS_INFO("height: %i , width: %i", height, width);
-  ROS_INFO("seq: %i", msg->header.seq); // starting with 732
-  ROS_INFO("time stamp: %d", msg->header.stamp); // starting with 1616052219
-  ROS_INFO("frame_id: %s\n", msg->header.frame_id.c_str()); // ?
+  cv::convertScaleAbs(float_img, mono8_img, 100, 0.0);
 }
 
-// function for the image writing process
-void imageWrite(const sensor_msgs::ImageConstPtr& msg) {
+void depthCallback(const sensor_msgs::ImageConstPtr& original_image) {
+  cv_bridge::CvImagePtr cv_ptr;
+  //Convert from the ROS image message to a CvImage suitable for working with OpenCV for processing
   try {
-    // convert image from ros msg to bgr8 (cv)
-    cv::Mat imgw = cv_bridge::toCvShare(msg, "bgr8")->image;
-    // const ros::Time time(time_stamps[i]); // creates ros time stamp from int
-    int stamp = msg->header.stamp.toSec(); // cut off decimals of seconds and convert to int
-    // loop through all poses
-    for (int i = 0; i < n_poses; i++) {
-      if (stamp == time_stamps[i]) { // if time stamp == any of pre-entered stamp (there seems to be a faster function for this (std::any_of))
-        // check whether already saved an image with the same time stamp
-        if (time_stamps[i] == time_stamps_saved[i]) {} // do nothign if already saved
-        else {
-          // create filename
-          stringstream ss;
-          string name = "/home/nico/catkin_ws/src/frame_reader/cal_imgs/pose_";
-          string type = ".jpg";
-          ss << name << (pose) << type; // pose = counter, set at start
-          string filename = ss.str();
-          ss.str("");
-          // write img
-          imwrite(filename, imgw);
-          // put time stamp into list of already saved imgs to avoid doublets
-          time_stamps_saved[i] = time_stamps[i];
-          std::cout << "Pose_" << pose << " img written." << endl;
-          pose++;
-        }
-      }
-    }
+    //Always copy, returning a mutable CvImage
+    //OpenCV expects color images to use BGR channel order.
+    cv_ptr = cv_bridge::toCvCopy(original_image);
   }
   catch (cv_bridge::Exception &e) {
-    ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str()); //could also be anything of the above
+    //if there is an error during conversion, display it
+    ROS_ERROR("tutorialROSOpenCV::main.cpp::cv_bridge exception: %s", e.what());
+    return;
   }
-  //ROS_INFO("frame_id: %s ; time: %s", frame_id, time);
-  //ROS_INFO("height: %i , width: %i", msg->height, msg->width);
-  //ROS_INFO("seq: %i", msg->header.seq); // starting with 732
-  ROS_INFO("time stamp: %d", msg->header.stamp); // starting with 1616052219
-  //ROS_INFO("frame_id: %s\n", msg->header.frame_id.c_str()); //
+
+  // now converting from 16bit (depth img) to 8bit, loss is small
+  cv::Mat depth_float_img = cv_ptr->image;
+  cv::Mat depth_mono8_img;
+  depthToCV8UC1(depth_float_img, depth_mono8_img);
+  cv::imshow("frame", depth_mono8_img);
+  cv::waitKey(1);
+}
+
+
+// writing depth image
+void depthImageWrite(const sensor_msgs::ImageConstPtr& msg) {
+  try {
+    // converting from 16 bit to 8 bit :/ ... 5 bits are apparently not used for image data
+    // thus only 11 to 8 conversion ... still lossy
+    cv::Mat depth_float_img = cv_bridge::toCvCopy(msg)->image;
+    cv::Mat img;
+    depthToCV8UC1(depth_float_img, img);
+
+    // create filename
+    stringstream ss;
+    string name = "/frame_reader/cal_imgs/new/depth/pose_";
+    string type = ".jpg";
+    ss << path << name << (pose_depth) << type; // pose = counter, set at start
+    string filename = ss.str();
+    ss.str("");
+    // write img
+    imwrite(filename, img);
+    std::cout << "depth: Pose_" << pose_depth << " img written." << endl;
+    pose_depth++;
+  }
+  catch (cv_bridge::Exception &e) {
+    std::cout << "Could not write depth: Pose_" << pose_depth-1 << " image." << endl;
+  }
+}
+
+
+// ir image writing
+void irImageWrite(const sensor_msgs::ImageConstPtr& msg) {
+  try {
+    // convert image from ros msg to bgr8 (cv)
+    cv::Mat img = cv_bridge::toCvShare(msg, "bgr8")->image;
+
+    // create filename
+    stringstream ss;
+    string name = "/frame_reader/cal_imgs/new/ir/pose_";
+    string type = ".jpg";
+    ss << path << name << (pose_ir) << type; // pose = counter, set at start
+    string filename = ss.str();
+    ss.str("");
+    // write img
+    imwrite(filename, img);
+    std::cout << "ir: Pose_" << pose_ir << " img written." << endl;
+    pose_ir++;
+  }
+  catch (cv_bridge::Exception &e) {
+    std::cout << "Could not write ir: Pose_" << pose_ir-1 << " image." << endl;
+  }
+}
+
+
+// rgb image writing
+void rgbImageWrite(const sensor_msgs::ImageConstPtr& msg) {
+  try {
+    // convert image from ros msg to bgr8 (cv)
+    cv::Mat img = cv_bridge::toCvShare(msg, "bgr8")->image;
+
+    // create filename
+    stringstream ss;
+    string name = "/frame_reader/cal_imgs/new/rgb/pose_";
+    string type = ".jpg";
+    ss << path << name << (pose_rgb) << type; // pose = counter, set at start
+    string filename = ss.str();
+    ss.str("");
+    // write img
+    imwrite(filename, img);
+    std::cout << "rgb: Pose_" << pose_rgb << " img written." << endl;
+    pose_rgb++;
+  }
+  catch (cv_bridge::Exception &e) {
+    std::cout << "Could not write rgb: Pose_" << pose_rgb-1 << " image." << endl;
+  }
 }
 
 int main(int argc, char** argv) {
   ros::init(argc, argv,"frame_reader");
   ros::NodeHandle nh;
+  image_transport::ImageTransport it(nh);
 
-  // uncomment to: read only
-  cv::namedWindow("frame");
-  ros::Subscriber sub = nh.subscribe("/rgb/image_raw", 1, imageCallback); // apparently transport should always be used for imgs
+  // callbacks. each image type has its own saving callback
+  image_transport::Subscriber rgb_sub = it.subscribe("/k4a/rgb/image_raw", 1, rgbImageWrite);
+  image_transport::Subscriber ir_sub = it.subscribe("/k4a/ir/image_raw", 1, irImageWrite);
+  image_transport::Subscriber depth_sub = it.subscribe("/k4a/depth/image_raw", 1, depthImageWrite);
 
-  // uncomment to: write imgs
-  //image_transport::ImageTransport it(nh);
-  //image_transport::Subscriber sub = it.subscribe("k4a/rgb/image_raw", 1, imageWrite);
-
-  // subscribing at frequency
-  // while loop needed
-  ros::Rate rate(4 /*Hz*/);
-  while (ros::ok()) {
-    ros::spinOnce(); // use this when using rosrate
-    rate.sleep();
+  // wait until any key is pressed
+  cout << "\n\nPress any key to save images.\n";
+  while (cin.ignore()) {
+    ros::spinOnce(); //call callbacks
   }
-  //ros::spin(); // use this without rosrate, loops automatically
-  cv::destroyWindow("frame");
+  //ros::spin();
   return 0;
 }
