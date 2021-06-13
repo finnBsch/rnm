6bbcb29045721a76f8a613b63b86424d043f57bf
konrad <konrad.reuter@tuhh.de>
2021-06-13 21:32:59 +0200

    implemented the stitching node as class

--- point_cloud_registration/src/ApproximateTimeSynchronizer.cpp
+++ point_cloud_registration/src/ApproximateTimeSynchronizer.cpp
@@ -7,12 +7,17 @@
 
 ros::Publisher publisher;
 
-void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg, const sensor_msgs::JointStateConstPtr& joint_states)
+void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg, const sensor_msgs::JointStateConstPtr& joint_states, int& msg_counter)
 {
-  point_cloud_registration::PCJScombined PCJS;
-  PCJS.PC = *cloud_msg;
-  PCJS.JS = *joint_states;
-  publisher.publish(PCJS);
+  if (msg_counter % 20 == 0){
+    point_cloud_registration::PCJScombined PCJS;
+    PCJS.PC = *cloud_msg;
+    PCJS.JS = *joint_states;
+    publisher.publish(PCJS);
+    msg_counter++;
+  } else {
+    msg_counter++;
+  }
 }
 
 int
@@ -20,6 +25,7 @@ main (int argc, char** argv)
 {
   ros::init (argc, argv, "Synchronizer");
   ros::NodeHandle nh;
+  int msg_count = 0;
   //Let the publisher publish on topic transformed_clouds
   publisher = nh.advertise<point_cloud_registration::PCJScombined> ("PCJScombined", 50);
 
@@ -32,6 +38,6 @@ main (int argc, char** argv)
   //again a large queue size to make sure no messages get lost
   typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::PointCloud2, sensor_msgs::JointState> MySyncPolicy;
   message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(500), filtered_cloud, joint_states);
-  sync.registerCallback(boost::bind(&cloud_cb, _1, _2));
+  sync.registerCallback(boost::bind(&cloud_cb, _1, _2, msg_count));
   ros::spin ();
 }
---
+++ point_cloud_registration/src/PCStitch.cpp
@@ -0,0 +1,171 @@
+#include <iostream>
+#include <ros/ros.h>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/registration/icp.h>
+#include <pcl/common/transforms.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/crop_box.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <eigen3/Eigen/Dense>
+#include <sensor_msgs/JointState.h>
+#include <sensor_msgs/PointCloud2.h>
+#include "forward_kin/get_endeffector.h"
+#include "point_cloud_registration/PCJScombined.h"
+
+class PCStitch
+{
+ public:
+  PCStitch(ros::NodeHandle& nh, Eigen::Matrix4f handeye):
+        nh_ (nh),
+        handeye_ (handeye),
+        pub_ (nh.advertise<sensor_msgs::PointCloud2> ("stitched_cloud", 1))
+  {
+    stitched_cloud_ = pcl::PointCloud<pcl::PointXYZRGB>::Ptr(new pcl::PointCloud<pcl::PointXYZRGB> ());
+    cloud_xyz_ = pcl::PointCloud<pcl::PointXYZRGB>::Ptr(new pcl::PointCloud<pcl::PointXYZRGB> ());
+  }
+
+ private:
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr stitched_cloud_;
+  pcl::PCLPointCloud2* received_cloud_{};
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_xyz_;
+  ros::NodeHandle nh_;
+  ros::Publisher pub_;
+  sensor_msgs::JointState joint_states_;
+
+  Eigen::Matrix4f handeye_;
+  const float leaf_size = 0.005;
+  const Eigen::Vector4f xyz_min = {0.15, -0.5, 0.02, 1.0};
+  const Eigen::Vector4f xyz_max = {0.7, 0.5, 0.3, 1.0};
+  const int mean_k = 50;
+  const double std_dev_max = 0.5;
+  const int icp_max = 10;
+  const double icp_epsilon = 1e-20;
+
+
+  void receiveCloud(const point_cloud_registration::PCJScombined::ConstPtr& PCJS){
+    received_cloud_ = new pcl::PCLPointCloud2;
+    pcl_conversions::toPCL(PCJS->PC, *received_cloud_);
+    joint_states_ = PCJS->JS;
+  }
+
+  void subsample(){
+    pcl::PCLPointCloud2ConstPtr cloudPtr(received_cloud_);
+    pcl::PCLPointCloud2 pc2_cloud_subsample;
+
+    // Perform the actual filtering
+    pcl::VoxelGrid<pcl::PCLPointCloud2> vg;
+    vg.setInputCloud (cloudPtr);
+    vg.setLeafSize (leaf_size, leaf_size, leaf_size);
+    vg.filter (pc2_cloud_subsample);
+
+    pcl::fromPCLPointCloud2(pc2_cloud_subsample, *cloud_xyz_);
+  }
+
+  void outlierRemoval(){
+    pcl::StatisticalOutlierRemoval<pcl::PointXYZRGB> sor;
+    sor.setInputCloud(cloud_xyz_);
+    sor.setMeanK(mean_k);
+    sor.setStddevMulThresh(std_dev_max);
+    sor.filter(*cloud_xyz_);
+  }
+
+  void transform(){
+    forward_kin::get_endeffector srv;
+
+    ros::ServiceClient client = nh_.serviceClient<forward_kin::get_endeffector>("forward_kin_node/get_endeffector");
+    boost::array<double, 7> angles = {joint_states_.position[0], joint_states_.position[1],joint_states_.position[2],
+                                      joint_states_.position[3], joint_states_.position[4],joint_states_.position[5],
+                                      joint_states_.position[6]};
+
+    srv.request.joint_angles = angles;
+
+    // --------------- GET A MATRIX -------------------
+    client.call(srv);
+
+    // get the current Transformation matrix Matrix from the forward_kin node
+    Eigen::Matrix4f transform;
+    transform << srv.response.layout.data[0],srv.response.layout.data[1],srv.response.layout.data[2],srv.response.layout.data[3],
+        srv.response.layout.data[4],srv.response.layout.data[5],srv.response.layout.data[6],srv.response.layout.data[7],
+        srv.response.layout.data[8],srv.response.layout.data[9],srv.response.layout.data[10],srv.response.layout.data[11],
+        srv.response.layout.data[12],srv.response.layout.data[13],srv.response.layout.data[14],srv.response.layout.data[15];
+    transform = transform*handeye_;
+    
+    //Transformation of the filtered cloud
+    pcl::transformPointCloud (*cloud_xyz_, *cloud_xyz_, transform);
+  }
+
+  void crop(){
+    pcl::CropBox<pcl::PointXYZRGB> crop;
+    crop.setMin(Eigen::Vector4f(xyz_min));
+    crop.setMax(Eigen::Vector4f(xyz_max));
+    crop.setInputCloud(cloud_xyz_);
+    crop.filter(*cloud_xyz_);
+  }
+
+  void ICP(){
+    if (stitched_cloud_->empty()){
+      *stitched_cloud_ = *cloud_xyz_;
+    }else{
+      //Set clouds for the ICP algorithm
+      pcl::PointCloud<pcl::PointXYZRGB>::Ptr goal_cloud (new pcl::PointCloud<pcl::PointXYZRGB> ());
+      pcl::IterativeClosestPoint<pcl::PointXYZRGB, pcl::PointXYZRGB> icp;
+      icp.setInputSource(cloud_xyz_);
+      icp.setInputTarget(stitched_cloud_);
+
+      //ICP parameters (examples), we still need to adjust them
+      //icp.setEuclideanFitnessEpsilon(1);
+      icp.setTransformationEpsilon(icp_epsilon);
+      icp.setMaximumIterations(icp_max);
+
+      //align new cloud to stitched cloud and add to the total stitched cloud.
+      icp.align(*goal_cloud);
+      *stitched_cloud_ += *goal_cloud;
+    }
+  }
+
+  void publishCloud(){
+    sensor_msgs::PointCloud2 output;
+    pcl::toROSMsg(*stitched_cloud_, output);
+    pub_.publish (output);
+  }
+
+ public:
+  void addCloud(const point_cloud_registration::PCJScombined::ConstPtr& PCJS){
+    receiveCloud(PCJS);
+    subsample();
+    outlierRemoval();
+    transform();
+    crop();
+    ICP();
+    publishCloud();
+  }
+};
+
+void cloud_cb (const point_cloud_registration::PCJScombined::ConstPtr& PCJS, PCStitch& pcs){
+  pcs.addCloud(PCJS);
+}
+
+int
+main (int argc, char** argv)
+{
+  const int queue_size = 50;
+
+
+  ros::init (argc, argv, "PCStitch");
+  ros::NodeHandle nh;
+
+  Eigen::Matrix4f handeye;
+  handeye << -0.0342177, -0.0224303,   0.999141,  0.0357424,
+      -0.998979, -0.0285611,  -0.034854, -0.0232153,
+      0.0293307,  -0.999324, -0.0214294,  0.0547717,
+      0,          0,          0,          1;
+  PCStitch pcs(nh, handeye);
+
+
+  ros::Subscriber sub = nh.subscribe<point_cloud_registration::PCJScombined>("/PCJScombined", queue_size, boost::bind(&cloud_cb, _1, pcs));
+  ros::spin ();
+}
+
