0c7418b9506d6561b9840ebd96ea553b9a020ac0
finn <finn.lukas.busch@gmail.com>
2021-07-05 10:28:53 +0200

    goal pos working

--- path_planning/src/move_to_point.cpp
+++ path_planning/src/move_to_point.cpp
@@ -11,8 +11,8 @@ using namespace ruckig;
 using namespace Eigen;
 using namespace std;
 ros::Publisher* traj_pub;
-const float fac = 0.04;
-const float fac2 = 0.003;
+const float fac = 0.07;
+const float fac2 = 0.07;
 std::vector<float> max_vels = {2.175f*fac, 2.175f*fac, 2.175f*fac, 2.175f*fac, 2.61f*fac, 2.61f*fac, 2.61f*fac};
 std::vector<float> max_accs = {15.0f*fac2, 7.5f*fac2, 10.0f*fac2, 12.5f*fac2, 15.0f*fac2, 20.0f*fac2, 20.0f*fac2};
 
@@ -37,18 +37,19 @@ void callback(const std_msgs::Float64MultiArray& msg) {
     input.max_velocity[i] = max_vels[i];
   }
   OutputParameter<7> output;
-  output.trajectory.calculate(input, 0.001);
+  output.trajectory.calculate<false, true>(input, (double)0.001);
   vector<array<double, 7>> goal_trajectory;
   trajectory_msgs::JointTrajectory traj_msg;
-
-  size_t sample_count = std::ceil(output.getDuration() / 0.001);
+  size_t sample_count = std::ceil(output.trajectory.get_duration() / 0.001);
   array<double, 7> one_point;
   for (size_t sample = 0; sample <= sample_count; ++sample)
   {
     // always sample the end of the trajectory as well
-    double t = std::min(output.getDuration(), sample * 0.001);
-    Vector<double,7> np;
-    output.trajectory.at_time(t,np);
+    double t = std::min(output.trajectory.get_duration(), sample * 0.001);
+    array<double,7> np;
+    array<double,7> np_;
+    array<double,7> np__;
+    output.trajectory.at_time(t,np,np_,np__);
     for (size_t j = 0; j < 7; ++j)
     {
       one_point[j] = np[j];
@@ -56,9 +57,9 @@ void callback(const std_msgs::Float64MultiArray& msg) {
     trajectory_msgs::JointTrajectoryPoint pt;
     pt.positions.assign(one_point.begin(), one_point.end());
     traj_msg.points.push_back(pt);
-    traj_pub->publish(traj_msg);
-    ros::spinOnce();
   }
+  traj_pub->publish(traj_msg);
+  ros::spinOnce();
 }
 int main(int argc, char** argv) {
   ros::init(argc, argv, "move_to_point_node");
