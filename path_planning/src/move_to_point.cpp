c8c77f013136c13ab3c4258a4e1b48eea80d66ac
finn <finn.lukas.busch@gmail.com>
2021-07-05 13:11:18 +0200

    move to point jerk free

--- path_planning/src/move_to_point.cpp
+++ path_planning/src/move_to_point.cpp
@@ -11,21 +11,22 @@ using namespace ruckig;
 using namespace Eigen;
 using namespace std;
 ros::Publisher* traj_pub;
-const float fac = 0.07;
-const float fac2 = 0.07;
+const float fac = 0.05;
+const float fac2 = 0.01;
 std::vector<float> max_vels = {2.175f*fac, 2.175f*fac, 2.175f*fac, 2.175f*fac, 2.61f*fac, 2.61f*fac, 2.61f*fac};
 std::vector<float> max_accs = {15.0f*fac2, 7.5f*fac2, 10.0f*fac2, 12.5f*fac2, 15.0f*fac2, 20.0f*fac2, 20.0f*fac2};
 
 void callback(const std_msgs::Float64MultiArray& msg) {
-  //auto start = *(ros::topic::waitForMessage<sensor_msgs::JointState>("/franka_state_controller/joint_states_desired",ros::Duration(10)));
-  auto start = *(ros::topic::waitForMessage<sensor_msgs::JointState>("/joint_states",ros::Duration(10)));
+  auto start = *(ros::topic::waitForMessage<sensor_msgs::JointState>("/franka_state_controller/joint_states_desired",ros::Duration(10)));
+  //auto start = *(ros::topic::waitForMessage<sensor_msgs::JointState>("/joint_states",ros::Duration(10)));
   Ruckig<7> otg {0.001}; // Number DoFs; control cycle in [s]
   InputParameter<7> input; // Number DoFs
   input.current_velocity = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
   input.current_acceleration = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
   input.target_velocity = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
   input.target_acceleration = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-  input.max_jerk = {10.0, 10.0, 10.0, 4.0, 4.0, 4.0, 4.0};
+  double fac3 = 0.01;
+  input.max_jerk = {fac3, fac3,fac3,fac3,fac3,fac3,fac3};
   for(int j = 0; j < 7; j++){
     input.current_position[j] = start.position[j];
   }
@@ -38,22 +39,27 @@ void callback(const std_msgs::Float64MultiArray& msg) {
     input.max_velocity[i] = max_vels[i];
   }
   OutputParameter<7> output;
-  output.trajectory.calculate<false, true>(input, (double)0.001);
   vector<array<double, 7>> goal_trajectory;
   trajectory_msgs::JointTrajectory traj_msg;
-  size_t sample_count = std::ceil(output.trajectory.get_duration() / 0.001);
   array<double, 7> one_point;
-  for (size_t sample = 0; sample <= sample_count; ++sample)
+  for (size_t j = 0; j < 7; ++j)
   {
-    // always sample the end of the trajectory as well
-    double t = std::min(output.trajectory.get_duration(), sample * 0.001);
-    array<double,7> np;
-    array<double,7> np_;
-    array<double,7> np__;
-    output.trajectory.at_time(t,np,np_,np__);
+    one_point[j] = input.current_position[j];
+  }
+  trajectory_msgs::JointTrajectoryPoint pt;
+  pt.positions.assign(one_point.begin(), one_point.end());
+  traj_msg.points.push_back(pt);
+  while (otg.update(input, output) == Result::Working) {
+    auto& p = output.new_position;
+    //std::cout << output.time << " " << p[0] << " " << p[1] << " " << p[2] << " " << p[3] << " " << p[4] << " " << p[5] << std::endl;
+
+    input.current_position = output.new_position;
+    input.current_velocity = output.new_velocity;
+    input.current_acceleration = output.new_acceleration;
+    array<double, 7> one_point;
     for (size_t j = 0; j < 7; ++j)
     {
-      one_point[j] = np[j];
+      one_point[j] = input.current_position[j];
     }
     trajectory_msgs::JointTrajectoryPoint pt;
     pt.positions.assign(one_point.begin(), one_point.end());
@@ -65,7 +71,7 @@ void callback(const std_msgs::Float64MultiArray& msg) {
 int main(int argc, char** argv) {
   ros::init(argc, argv, "move_to_point_node");
   ros::NodeHandle  nh("~");
-  traj_pub = new ros::Publisher(nh.advertise<trajectory_msgs::JointTrajectory>("/trajectory_sim", 10));
-  ros::Subscriber sub = nh.subscribe("/Scanner_node/move_command", 10, callback);
+  traj_pub = new ros::Publisher(nh.advertise<trajectory_msgs::JointTrajectory>("/trajectory", 10));
+  ros::Subscriber sub = nh.subscribe("/move_command", 10, callback);
   ros::spin();
 }
